8cb74b432c5cfdfdd02c52e7981a3b66fc7a0d32
Akula Srivarsha <akulasrivarsha28@gmail.com>
2026-05-21 21:52:07 +0530

    Implement longestCommonPrefix method in Solution class

---
+++ Length of Longest common prefix.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestCommonPrefix(self, arr1, arr2):
+        prefixes = set()
+        # Store all prefixes of arr1 numbers
+        for num in arr1:
+            s = str(num)
+            for i in range(1, len(s) + 1):
+                prefixes.add(s[:i])
+        ans = 0
+        # Check prefixes in arr2
+        for num in arr2:
+            s = str(num)
+            for i in range(1, len(s) + 1):
+                if s[:i] in prefixes:
+                    ans = max(ans, i)
+        return ans
