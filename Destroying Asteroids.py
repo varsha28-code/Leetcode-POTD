024b1f16bbeaa4d38f22b6b33b9905bf973c6801
Akula Srivarsha <akulasrivarsha28@gmail.com>
2026-05-31 14:46:06 +0530

    Implement asteroid destruction logic in Solution class

---
+++ Destroying Asteroids.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def asteroidsDestroyed(self, mass, asteroids):
+        """
+        :type mass: int
+        :type asteroids: List[int]
+        :rtype: bool
+        """
+        asteroids.sort()
+        for asteroid in asteroids:
+            if mass < asteroid:
+                return False
+            mass += asteroid
+        return True
