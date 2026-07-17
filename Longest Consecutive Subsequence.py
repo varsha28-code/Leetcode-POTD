fc71c8172fec5c44578d9f07b172a245c5e77e0a
Akula Srivarsha <akulasrivarsha28@gmail.com>
2026-07-17 09:40:24 +0530

    Implement longest consecutive subsequence algorithm

---
+++ Longest Consecutive Subsequence.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        num_set = set(nums)
+        longest = 0
+        for num in num_set:
+            if num - 1 not in num_set:
+                current = num
+                length = 1
+                while current + 1 in num_set:
+                    current += 1
+                    length += 1
+                longest = max(longest, length)
+        return longest
