45ffe738d584faf55aa0c76b49800cf2691a9463
Akula Srivarsha <akulasrivarsha28@gmail.com>
2026-07-19 17:31:59 +0530

    Create 1081. Smallest Subsequence of Distinct Characters.py

---
+++ 1081. Smallest Subsequence of Distinct Characters.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def smallestSubsequence(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        last = {}
+        for i, ch in enumerate(s):
+            last[ch] = i
+
+        stack = []
+        visited = set()
+
+        for i, ch in enumerate(s):
+            if ch in visited:
+                continue
+
+            while (
+                stack
+                and ch < stack[-1]
+                and last[stack[-1]] > i
+            ):
+                visited.remove(stack.pop())
+
+            stack.append(ch)
+            visited.add(ch)
+
+        return "".join(stack)
