727a7e8f31247cd0a37a8c068e22c991b7fa2528
Akula Srivarsha <akulasrivarsha28@gmail.com>
2026-07-26 19:11:28 +0530

    Optimize maximum product calculation for three numbers

--- maximum product of three numbers.py
+++ maximum product of three numbers.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        prod=1
-        for i in nums:
-            prod=prod*i
-        return prod
+        nums.sort()
+        product1 = nums[-1] * nums[-2] * nums[-3]
+        product2 = nums[0] * nums[1] * nums[-1]
+        return max(product1, product2)
